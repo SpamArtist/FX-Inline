da93cc663bd1fbaeb67518bc8f70b3208e73f9b5
Piyush <piyush.work@proton.me>
2026-03-12 12:47:24 +0000

    firefox specific

--- wxt.config.ts
+++ wxt.config.ts
@@ -33,6 +33,15 @@ export default defineConfig({
       extension_pages:
         "script-src 'self'; object-src 'self'; connect-src 'self' http://127.0.0.1:8787 http://localhost:8787 https://open.er-api.com http://127.0.0.1:3000 http://localhost:3000 ws://127.0.0.1:3000 ws://localhost:3000",
     },
+    browser_specific_settings: {
+      gecko: {
+        id: "fx-inline@xbotpc",
+        // @ts-ignore - WXT doesn't support this field yet
+        data_collection_permissions: {
+          required: ["none"],
+        },
+      },
+    },
   },
   vite: () => ({
     plugins: [
